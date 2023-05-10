1291489081d59bb35c41daa26fc1a54bb06c5d80
Sergejs Podolaks <sergejspodolaks@Sergejss-MacBook-Pro-2.local>
2023-05-10 10:07:10 +0300

    chat added

---
+++ server.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	chat.RegisterChatServiceServer(grpcServer, &s)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %s", err)
+	}
+
+
+}
